1fcbac8023e7e1b5ea6de5dae76fe1b63dabd8ac
Alberto Limón <119005684+albertolimongithub@users.noreply.github.com>
2024-06-19 22:43:15 +0000

    second commit

--- src/models.py
+++ src/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
+'''
 Base = declarative_base()
 
 class User(Base):
@@ -86,3 +87,53 @@ try:
 except Exception as e:
     print("There was a problem genering the diagram")
     raise e
+'''
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=False)
+    phone = Column(String(250), nullable=False)
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    img = Column(String(250), nullable=False)
+    title = Column(String(250), nullable=False)
+    content = Column(String(250), nullable=False)
+
+class Like(Base):
+    __tablename__ = 'like'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    id_post = Column(Integer, ForeignKey('post.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    id_post = Column(Integer, ForeignKey('post.id'))
+    text = Column(String(250), nullable=False)
+
+class Follower(Base):
+    __tablename__ = 'follower'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    username = Column(String(250), nullable=False)
+    
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
